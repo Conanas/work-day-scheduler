e6717abd414341e20b1b8fb53223953b36ceb589
Conanas <tmhaigh@gmail.com>
2020-09-04 09:19:24 +1000

    completed function to create layout display

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 var DayPlanner = {
     timeSlots: ["9AM", "10AM", "11AM", "12AM", "1AM", "2AM", "3AM", "4AM", "5AM"],
+    addButtonText: "Add",
+    createLayout: function() {
+
+        var layoutDisplay = $("#layout-display");
+        var thisAddButtonText = this.addButtonText;
+
+        this.timeSlots.forEach(function(timeSlot) {
+
+            // create timeslot div
+            var newTimeSlot = $("<div>");
+            newTimeSlot.addClass("time-slot");
+
+            // create time display
+            var newTime = $("<div>");
+            newTime.addClass("time-display");
+            newTime.text(timeSlot);
+            newTimeSlot.append(newTime);
+
+            // create text area
+            var newTextArea = $("<textarea>");
+            newTextArea.attr("name", "event-textarea");
+            newTextArea.attr("id", `${timeSlot}-textarea`);
+            newTimeSlot.append(newTextArea);
+
+            // create add event button
+            var newAddButton = $("<button>");
+            newAddButton.addClass("add-event-button");
+            newAddButton.text(thisAddButtonText);
+            newTimeSlot.append(newAddButton);
+
+            // append timeSlot div to layout display
+            layoutDisplay.append(newTimeSlot);
+        });
+    },
 }
 
-function createLayout() {
-
-}
+DayPlanner.createLayout();
