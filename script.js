2ff20bc2e327aa37e79eaf4f915878eb67ae0f51
Conanas <tmhaigh@gmail.com>
2020-09-06 14:06:07 +1000

    added save and load functionality

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
 var DayPlanner = {
-    slots: ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"],
-    hours: [9, 10, 11, 12, 13, 14, 15, 16, 17],
+    Slots: [
+        { slotString: "9AM", slotInt: 9, slotEvent: "" },
+        { slotString: "10AM", slotInt: 10, slotEvent: "" },
+        { slotString: "11AM", slotInt: 11, slotEvent: "" },
+        { slotString: "12PM", slotInt: 12, slotEvent: "" },
+        { slotString: "1PM", slotInt: 13, slotEvent: "" },
+        { slotString: "2PM", slotInt: 14, slotEvent: "" },
+        { slotString: "3PM", slotInt: 15, slotEvent: "" },
+        { slotString: "4PM", slotInt: 16, slotEvent: "" },
+        { slotString: "5PM", slotInt: 17, slotEvent: "" }
+    ],
     addButtonText: "Add",
-    day: moment().format("dddd"),
-    date: moment().format("Do"),
-    month: moment().format("MMMM"),
-    year: moment().format("YYYY"),
-
-    // don't allow to save in the past
-    savedEvents: [],
+    date: moment().format("dddd Do MMMM YYYY"),
 
     // display the date in the header
     displayDate: function() {
         var dateDisplay = $(".date-display");
-        dateDisplay.text(`${this.day} ${this.date} ${this.month} ${this.year}`);
+        dateDisplay.text(`${this.date}`);
+    },
+
+    checkNewDay: function() {
+        if (moment().isAfter(moment().hour(9))) {
+            // if not new day then load Slots
+            this.loadEvents();
+        } else {
+            // if new day then overwrite Slots
+            localStorage.setItem("dayPlannerEvents", JSON.stringify(DayPlanner.Slots));
+        }
+    },
+
+    // check if textarea is in the past present or future and return background color
+    backgroundColor: function(i) {
+        if (moment().isAfter(moment().hours(this.Slots[i].slotInt))) {
+            return "lightgrey";
+        } else if (moment().isSame(moment().hours(this.Slots[i].slotInt))) {
+            return "indianred";
+        } else {
+            return "lightgreen";
+        }
+    },
+
+    // save event button function
+    saveEvent: function(event) {
+        var hour = event.target.value;
+        var index = hour - 9;
+        var slotValue = $(`#slot-textarea-${hour}`).val();
+        DayPlanner.Slots[index].slotEvent = slotValue;
+        localStorage.setItem("dayPlannerEvents", JSON.stringify(DayPlanner.Slots));
+    },
+
+    // load events into DayPlanner savedEvents array
+    loadEvents: function() {
+        this.Slots = JSON.parse(localStorage.getItem("dayPlannerEvents"));
     },
 
     // create the time slots layout
     createLayout: function() {
 
-        // display todays date
         this.displayDate();
+        this.checkNewDay();
 
         // variables
         var layoutDisplay = $("#layout-display");
-        var thisHours = this.hours;
         var backgroundColor = "";
         var thisAddButtonText = this.addButtonText;
 
         // for each time slot create planner elements
-        for (var i = 0; i < this.slots.length; i++) {
+        for (var i = 0; i < this.Slots.length; i++) {
 
             // create slot row
             var newSlotRow = $("<div>");
@@ -45,10 +82,10 @@ var DayPlanner = {
             var newSlotLabel = $("<label>");
             newSlotLabel.addClass("slot-label");
             newSlotLabel.attr({
-                for: `slot-textarea-${this.slots[i]}`,
-                id: `slot-label-${this.slots[i]}`
+                for: `slot-textarea-${this.Slots[i].slotString}`,
+                id: `slot-label-${this.Slots[i].slotString}`
             });
-            newSlotLabel.text(this.slots[i]);
+            newSlotLabel.text(this.Slots[i].slotString);
             newSlotTimeCol.append(newSlotLabel);
 
             // create slot textarea column
@@ -56,21 +93,21 @@ var DayPlanner = {
             newSlotTextareaCol.addClass("col-6 col-sm-8 slot-input");
             newSlotRow.append(newSlotTextareaCol);
 
-            // create textarea for slot textarea column
-            var newSlotTextarea = $("<textarea>");
-            newSlotTextarea.addClass("slot-textarea");
-
             // assign a background color for textarea depending on time of day
             backgroundColor = DayPlanner.backgroundColor(i);
 
+            // create textarea for slot textarea column
+            var newSlotTextarea = $("<textarea>");
+            newSlotTextarea.addClass("slot-textarea");
             newSlotTextarea.attr({
-                name: `slot-textarea-${this.slots[i]}`,
-                id: `slot-textarea-${this.hours[i]}`,
+                name: `slot-textarea-${this.Slots[i].slotString}`,
+                id: `slot-textarea-${this.Slots[i].slotInt}`,
                 cols: "30",
                 rows: "3",
-                value: this.hours[i],
+                value: this.Slots[i].slotInt,
                 style: `background-color: ${backgroundColor};`
             });
+            newSlotTextarea.text(this.Slots[i].slotEvent);
             newSlotTextareaCol.append(newSlotTextarea);
 
             // create slot button colum
@@ -82,47 +119,15 @@ var DayPlanner = {
             var newSlotButton = $("<button>");
             newSlotButton.addClass("slot-button");
             newSlotButton.attr({
-                id: `slot-button-${this.slots[i]}`,
-                value: this.hours[i]
+                id: `slot-button-${this.Slots[i].slotString}`,
+                value: this.Slots[i].slotInt
             });
             newSlotButton.text(thisAddButtonText);
             newSlotButtonCol.append(newSlotButton);
 
             // click event listener for add button
-            newSlotButton.on("click", this.saveEvent);
-
+            newSlotButton.on("click", DayPlanner.saveEvent);
         }
-
-    },
-
-    // check if textarea is in the past present or future and return background color
-    backgroundColor: function(hour) {
-        if (moment().isAfter(moment().hours(this.hours[hour]))) {
-            return "lightgrey";
-        } else if (moment().isSame(moment().hours(this.hours[hour]))) {
-            return "indianred";
-        } else {
-            return "lightgreen";
-        }
-    },
-
-    // save event button function
-    saveEvent: function(event) {
-
-        var hour = event.target.value;
-        var slotValue = $(`#slot-textarea-${hour}`).val();
-        var Event = { eventHour: hour, eventSlotValue: slotValue };
-
-        // this.savedEvents.push(Event);
-
-        console.log(hour);
-        console.log(slotValue);
-        console.log(Event);
-        console.log(DayPlanner.savedEvents);
-        console.log(DayPlanner.slots);
-
-        // this.savedEvents.push({ hour, $(".slot-textarea").val() });
-
     }
 }
 
